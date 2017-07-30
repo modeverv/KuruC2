ce2497af662692f7372539c7bdcffeb5e6677d60
modeverv <modeverv@gmail.com>
2017-07-30 10:59:28 +0900

    enum

--- src/KuruC.c
+++ src/KuruC.c
@@ -11,6 +11,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
+#include <stdbool.h>
+
+
+#define TAX 0.08
+#define EXCISETAX 0.08
+#define AUTHOR "日本語太郎"
+
+typedef enum {
+	ONE, TWO,
+} Type;
 
 void get_data();
 void check_array();
@@ -18,17 +28,20 @@ void pri(int);
 void print_array(int[]);
 void file_write();
 void my_file_read();
+void check_define();
+void check_enum();
 
 int main(void) {
-	int a = 0b1111;
-	printf("%d", a);
-	printf("aaaa");
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	//int a = 0b1111;
+	//printf("%d", a);
+	//printf("aaaa");
+	//puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
 	//get_data();
 	//check_array();
 	//file_write();
-	my_file_read();
-
+	//my_file_read();
+	check_define();
+	check_enum();
 	return EXIT_SUCCESS;
 }
 
@@ -81,8 +94,21 @@ void my_file_read() {
 	char a[128];
 	FILE *file;
 	file = fopen("test.txt", "r");
+	// フォーマット指定しないとだめね～
 	fscanf(file, "%s", &a);
 	printf("%s", a);
 }
 
+void check_define() {
+	double price = 100.0;
+	price = price * (1 + TAX);
+	printf("%f\n", TAX);
+	printf("%f\n", EXCISETAX);
+	printf("%f\n", price);
+	printf("%s\n", AUTHOR);
+}
 
+void check_enum() {
+	Type e = ONE;
+	printf("%d\n", e);
+}
